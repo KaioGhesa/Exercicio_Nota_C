8ad50a3ff2112ab88b31241d1419fb21c3bf027e
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-10 18:53:39 -0300

    Add files via upload

---
+++ Nota/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Nota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            notas notas = new notas();
+
+            Console.Write("Digite a nota do aluno: ");
+            notas.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            notas.A1 = double.Parse(Console.ReadLine());
+            notas.A2 = double.Parse(Console.ReadLine());
+            notas.A3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Nota Final = " + 
+                notas.SomaNota().ToString("F2",CultureInfo.InvariantCulture));
+            if (notas.Aprovado())
+            {
+                Console.WriteLine("Aluno: " + notas.Nome + " aprovado!!");
+            }
+            else
+            {
+                Console.WriteLine("Aluno: " + notas.Nome + " Reprovado");
+                Console.WriteLine("Faltou: "
+                    + notas.NotaRestante().ToString("F2",CultureInfo.InvariantCulture) +
+                    " Pontos");
+            }
+        }
+    }
+}
---
+++ Nota/notas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nota
+{
+    internal class notas
+    {
+        public string Nome;
+        public double A1;
+        public double A2;
+        public double A3;
+
+        public double SomaNota()
+        {
+           return  A1 + A2 + A3;
+        }
+
+        public bool Aprovado()
+        {
+            if (SomaNota() >= 60.0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            } else
+            {
+                return 60.0 - SomaNota();
+            }
+        }
+    }
+}
